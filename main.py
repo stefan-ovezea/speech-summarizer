51d8b113d2a4efb56813b602001aef3c80b1c15b
Stefan Ovezea <sovezea@gmail.com>
2022-06-21 09:52:41 +0300

    Added initial html page

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+import os
+from flask import Flask, render_template
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+template_folder = os.path.abspath('.')
+print(os.path.abspath(''))
+app = Flask(__name__, template_folder=template_folder)
+
+@app.route('/')
+def home():
+    return render_template('./index.html')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
